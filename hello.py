a0f02d54e179f22b912b1e6b4a9639504fd8a5f5
fengyucai <i@caifengyu.net>
2017-01-05 00:25:18 +0800

    python3 print function

---
+++ hello.py
@@ -0,0 +1,13 @@
+# This program says hello
+print('Hello world!');
+print('What is your name?');
+myName  = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+age = input()
+print('You will be ' + str(int(age) + 1) + ' in a year.')
+
+
+
