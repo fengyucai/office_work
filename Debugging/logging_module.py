014ab1298f461505e813e4c40e470ca54142ce1e
fengyucai <i@caifengyu.net>
2017-01-15 18:07:27 +0800

    Python debugging: traceback, assert and logging

---
+++ Debugging/logging_module.py
@@ -0,0 +1,19 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,\
+format=' %(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug('Start of program')
+
+def factorial(n):
+    logging.debug('Start of fatorial( %s)' % (n))
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of fatorial(%s)' % (n))
+    return total
+
+print(factorial(4))
+logging.debug('End of program')
+        
+
