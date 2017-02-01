77cb0d9c6071e31e5310e1927b2f2a7f38dcbc50
fengyucai <i@caifengyu.net>
2017-02-01 16:21:01 +0800

    Count and Compare to detect anagram in O(n)

--- Algorithm/anagramDetection.py
+++ Algorithm/anagramDetection.py
@@ -31,4 +31,24 @@ def anagramSolution2(s1, s2):
             return False
     return True
 
-print(anagramSolution1('abcd', 'dcba'))
+# count and compare  O(n)
+def anagramSolution3(s1, s2):
+    c1 = [0]*26
+    c2 = [0]*26
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] += 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] += 1
+
+    j = 0
+    while j < 26:
+        if c1[j] == c2[j]:
+            j += 1
+        else:
+            return False
+    return True
+
+print(anagramSolution3('apple', 'pleap'))
