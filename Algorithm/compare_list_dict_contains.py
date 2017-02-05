948c2ee73af9e8aa7ee4179172daf0a19baaeb7e
fengyucai <i@caifengyu.net>
2017-02-06 05:03:00 +0800

    use timeit to compare contains operation of list and dict

---
+++ Algorithm/compare_list_dict_contains.py
@@ -0,0 +1,12 @@
+import random
+import timeit
+
+print('%+20s%16s%16s' % ('input size', 'list time', 'dict time'))
+for i in range(10000, 1000001, 20000):
+    t = timeit.Timer('random.randrange(%d) in x' % i,
+                     'from __main__ import random, x')
+    x = list(range(i))
+    list_time = t.timeit(number=1000)
+    x = {j : None for j in range(i)}
+    dict_time = t.timeit(number=1000)
+    print('%+20s: %10.3f secs %10.3f secs' % (i, list_time, dict_time))
